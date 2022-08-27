dae763cd6e2ff308aff9b06a8e5406749110d806
KOMORI Yusuke <little-forest@users.noreply.github.com>
2022-08-27 11:56:03 +0900

    Feature/fix timestamp (#34)
    * Change modified time to UnixNano

--- src/cmd/update.go
+++ src/cmd/update.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,7 +18,7 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"time"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -79,7 +79,7 @@ func updateHash(path string, alg *HashAlg, forceUpdate bool) (bool, string, erro
 		return false, "", err
 	}
 	size := fmt.Sprint(info.Size())
-	modTime := info.ModTime().Format(time.RFC3339Nano)
+	modTime := strconv.FormatInt(info.ModTime().UnixNano(), 10)
 
 	var changed bool
 	curHash := getXattr(file, alg.AttrName)
