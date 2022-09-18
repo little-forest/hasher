c145a8c4a68f8678f380ee467b99bf9f06791fb0
KOMORI Yusuke(y-komori@skuld) <komo@littleforest.jp>
2022-09-18 20:30:55 +0900

    Add testcase

--- src/core/dirdiff_test.go
+++ src/core/dirdiff_test.go
@@ -133,6 +133,28 @@ func TestDirDiffCompare_05(t *testing.T) {
 	assertFileDiff(t, "test0C", RENAMED, "test03", files[2])
 }
 
+func TestDirDiffCompare_06(t *testing.T) {
+	alg := NewDefaultHashAlg()
+	meDir, otherDir := prepareDirDiffTest_06(t, alg)
+
+	me, err := NewDirDiff(meDir, alg)
+	assert.NoError(t, err)
+	other, err := NewDirDiff(otherDir, alg)
+	assert.NoError(t, err)
+
+	me.Compare(other)
+
+	// assert
+	files := me.GetSortedChildren()
+	assert.Equal(t, 6, len(files))
+	assertFileDiff(t, "test01", SAME, "test01", files[0])
+	assertFileDiff(t, "test02", NOT_SAME_NEW, "test02", files[1])
+	assertFileDiff(t, "test04", NOT_SAME, "test04", files[2])
+	assertFileDiff(t, "test05", ADDED, "", files[3])
+	assertFileDiff(t, "test06", REMOVED, "", files[4])
+	assertFileDiff(t, "test07", RENAMED, "test03", files[5])
+}
+
 // DirDiff test pattern1
 //
 //	[=] test01 <-> test01
@@ -281,6 +303,51 @@ func prepareDirDiffTest_05(t *testing.T, alg *HashAlg) (string, string) {
 	copyFile(t, otherDir, meDir, "test03")
 	// nolint:errcheck
 	os.Rename(filepath.Join(meDir, "test03"), filepath.Join(meDir, "test0C"))
+
+	return meDir, otherDir
+}
+
+// DirDiff test pattern6
+//
+//	[=] test01 <-> test01
+//	[>] test02 <-> test02
+//	[R] test07 <-> test03
+//	[~] test04 <-> test04
+//	[+] test05 <->
+//	[-]        <-> test06
+func prepareDirDiffTest_06(t *testing.T, alg *HashAlg) (string, string) {
+	t.Helper()
+
+	meDir := t.TempDir()
+	otherDir := t.TempDir()
+
+	mPath1 := filepath.Join(meDir, "test01")
+	mPath2 := filepath.Join(meDir, "test02")
+	mPath4 := filepath.Join(meDir, "test04")
+	mPath5 := filepath.Join(meDir, "test05")
+
+	oPath2 := filepath.Join(otherDir, "test02")
+	oPath3 := filepath.Join(otherDir, "test03")
+	oPath4 := filepath.Join(otherDir, "test04")
+	oPath6 := filepath.Join(otherDir, "test06")
+
+	// me
+	makeDummyFile(t, mPath1, &alg.Alg)
+	makeDummyFile(t, mPath2, &alg.Alg)
+	makeDummyFile(t, mPath4, &alg.Alg)
+	makeDummyFile(t, mPath5, &alg.Alg)
+
+	// other
+	copyFile(t, meDir, otherDir, "test01")
+	makeDummyFile(t, oPath2, &alg.Alg)
+	touchDelta(t, mPath2, oPath2, time.Minute)
+	makeDummyFile(t, oPath3, &alg.Alg)
+	copyFile(t, otherDir, meDir, "test03")
+	// nolint:errcheck
+	os.Rename(filepath.Join(meDir, "test03"), filepath.Join(meDir, "test07"))
+	makeDummyFile(t, oPath4, &alg.Alg)
+	touchDelta(t, mPath4, oPath4, 0)
+	makeDummyFile(t, oPath6, &alg.Alg)
 
 	return meDir, otherDir
 }
