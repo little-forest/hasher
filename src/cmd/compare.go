764ba840fc301b57f01b150ccad59c68f8e52842
KOMORI Yusuke(y-komori@skuld) <komo@littleforest.jp>
2022-10-29 19:59:49 +0900

    Improve compare cmd

--- src/cmd/compare.go
+++ src/cmd/compare.go
@@ -24,11 +24,17 @@ import (
 
 const Flag_Compare_Source = "source"
 const Flag_Compare_Target = "target"
+const Flag_Compare_ShowExistsOnly = "show-exists-only"
+const Flag_Compare_ShowMissingOnly = "show-missing-only"
+
+const SHOW_ALWAYS = 0
+const SHOW_EXISTS_ONLY = 1
+const SHOW_MISSING_ONLY = 2
 
 // compareCmd represents the compare command
 var compareCmd = &cobra.Command{
 	Use:   "compare",
-	Short: "",
+	Short: "Compare hashes",
 	Long:  ``,
 	RunE:  statusWrapper.RunE(runCompare),
 }
@@ -38,11 +44,26 @@ func init() {
 
 	compareCmd.Flags().StringP(Flag_Compare_Source, "s", "", "source hash file")
 	compareCmd.Flags().StringP(Flag_Compare_Target, "t", "", "target hash file")
+	compareCmd.Flags().BoolP(Flag_Compare_ShowExistsOnly, "e", false, "show exist files only")
+	compareCmd.Flags().BoolP(Flag_Compare_ShowMissingOnly, "m", false, "show missing files only")
 }
 
 func runCompare(cmd *cobra.Command, args []string) (int, error) {
 	sourceFile, _ := cmd.Flags().GetString(Flag_Compare_Source)
 	targetFile, _ := cmd.Flags().GetString(Flag_Compare_Target)
+	showExistsOnly, _ := cmd.Flags().GetBool(Flag_Compare_ShowExistsOnly)
+	showMissingOnly, _ := cmd.Flags().GetBool(Flag_Compare_ShowMissingOnly)
+
+	// check options
+	if showExistsOnly && showMissingOnly {
+		return 1, fmt.Errorf("Can't specify both -e and -m")
+	}
+	showMode := SHOW_ALWAYS
+	if showExistsOnly {
+		showMode = SHOW_EXISTS_ONLY
+	} else if showMissingOnly {
+		showMode = SHOW_MISSING_ONLY
+	}
 
 	srcHashData, err := core.LoadHashData(sourceFile)
 	if err != nil {
@@ -54,21 +75,27 @@ func runCompare(cmd *cobra.Command, args []string) (int, error) {
 		return 1, err
 	}
 
-	result, err := doCompare(srcHashData, targetHashData)
+	result, err := doCompare(srcHashData, targetHashData, showMode)
 	return result, err
 }
 
-func doCompare(src *core.HashStore, target *core.HashStore) (int, error) {
+func doCompare(src *core.HashStore, target *core.HashStore, showMode int) (int, error) {
 	for _, hash := range src.Values() {
 		sames := target.Get(hash.String())
 
-		fmt.Printf("%s\t%d", hash.Path, len(sames))
-		if len(sames) > 0 {
-			for _, s := range sames {
-				fmt.Printf("\t%s", s.Path)
-			}
+		if len(sames) > 0 && showMode != SHOW_MISSING_ONLY {
+			fmt.Println(makeResult(hash, sames))
+		} else if len(sames) == 0 && showMode != SHOW_EXISTS_ONLY {
+			fmt.Println(makeResult(hash, sames))
 		}
-		fmt.Printf("\n")
 	}
 	return 0, nil
+}
+
+func makeResult(hash *core.Hash, sames []*core.Hash) string {
+	result := fmt.Sprintf("%s\t%d", hash.Path, len(sames))
+	for _, s := range sames {
+		result += "\t" + s.Path
+	}
+	return result
 }
